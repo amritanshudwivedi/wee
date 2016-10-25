6e4b9a413f731f43cd2716987825794c5885c78d
Amritanshu <amritanshu@grappus.com>
2016-10-25 09:59:18 +0530

    interpreter evaluating simple expressions without operator presedence

---
+++ interpreter.py
@@ -0,0 +1,157 @@
+INTEGER, PLUS, MINUS, MULTIPLY, DIVIDE, EOF = 'INTEGER', 'PLUS', 'MINUS', 'MULTIPLY', 'DIVIDE', 'EOF'
+
+class Token(object):
+
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def __str__(self):
+        """String representation of the class instance.
+
+        Examples:
+            Token(INTEGER, 3)
+            Token(PLUS, '+')
+        """
+        return 'Token({type}, {value})'.format(
+            type=self.type,
+            value=self.value
+        )
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Lexer(object):
+
+    def __init__(self, text):
+        self.text = text 
+        self.pos = 0
+        self.current_char = self.text[self.pos]
+
+    def error(self):
+        raise Exception('invalid character')
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def integer(self):
+        """Return a (multidigit) integer consumed from the input."""
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
+
+    def advance(self):
+        """Advance the 'pos' pointer and set the 'current_char' variable."""
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None
+        else:
+            self.current_char = self.text[self.pos]
+
+    def get_next_token(self):
+        while self.current_char is not None:
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
+
+            if self.current_char.isdigit():
+                return Token(INTEGER, self.integer())
+
+            if self.current_char == '+':
+                self.advance()
+                return Token(PLUS, '+')
+
+            if self.current_char == '-':
+                self.advance()
+                return Token(MINUS, '-')
+
+            if self.current_char == '*':
+                self.advance()
+                return Token(MULTIPLY, '*')
+
+            if self.current_char == '/':
+                self.advance()
+                return Token(DIVIDE, '/')
+            self.error()
+        return Token(EOF, None)
+
+
+class Interpreter(object):
+
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.current_token = self.lexer.get_next_token()
+
+    def error(self, where):
+        raise Exception('Error parsing input: {0}'.format(where))
+
+    def eat(self, token_type):
+        # compare the current token type with the passed token
+        # type and if they match then "eat" the current token
+        # and assign the next token to the self.current_token,
+        # otherwise raise an eat exception.
+        if self.current_token.type == token_type:
+            self.current_token = self.lexer.get_next_token()
+        else:
+            self.error('eat')
+
+    def factor(self):
+        token = self.current_token
+        self.eat(INTEGER)
+        return token.value
+
+    def expr(self):
+
+        # we expect the current token to be a single-digit integer
+        result = self.factor()
+
+        while self.current_token.type in (PLUS, MINUS, MULTIPLY,DIVIDE):
+            token = self.current_token
+            if token.type == PLUS:
+                self.eat(PLUS)
+                result += self.factor()
+            elif token.type == MINUS:
+                self.eat(MINUS)
+                result -= self.factor()
+            elif token.type == MULTIPLY:
+                self.eat(MULTIPLY)
+                result *= self.factor()
+            elif token.type == DIVIDE:
+                self.eat(DIVIDE)
+                result /= self.factor()
+        return result
+
+
+def main():
+    while True:
+        try:
+            text = raw_input('calc>')
+        except EOFError:
+            break
+        if not text:
+            continue
+        lexer = Lexer(text)
+        interpreter = Interpreter(lexer)
+        result = interpreter.expr()
+        print result
+    # while True:
+    #     try:
+    #         text = raw_input('calc>')
+    #     except EOFError:
+    #         break
+    #     if not text:
+    #         continue
+    #     lexer = Lexer(text)
+    #     current_token = lexer.get_next_token()
+    #     while current_token.type != EOF:
+    #         print(current_token)
+    #         current_token = lexer.get_next_token()
+
+
+
+if __name__ == '__main__':
+    main()
