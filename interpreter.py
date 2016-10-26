e8f2d45f0bfd6cb064b477c6d56f7b552a347186
Amritanshu <amritanshu@grappus.com>
2016-10-26 09:33:10 +0530

    expressions now support paranthesis

--- interpreter.py
+++ interpreter.py
@@ -1,4 +1,5 @@
-INTEGER, PLUS, MINUS, MULTIPLY, DIVIDE, EOF = 'INTEGER', 'PLUS', 'MINUS', 'MULTIPLY', 'DIVIDE', 'EOF'
+INTEGER, PLUS, MINUS, MULTIPLY, DIVIDE, LPAREN, RPAREN, EOF = (
+    'INTEGER', 'PLUS', 'MINUS', 'MULTIPLY', 'DIVIDE', '(', ')', 'EOF')
 
 class Token(object):
 
@@ -76,6 +77,14 @@ class Lexer(object):
             if self.current_char == '/':
                 self.advance()
                 return Token(DIVIDE, '/')
+
+            if self.current_char == '(':
+                self.advance()
+                return Token(LPAREN, '(')
+
+            if self.current_char == ')':
+                self.advance()
+                return Token(RPAREN, ')')
             self.error()
         return Token(EOF, None)
 
@@ -101,8 +110,14 @@ class Interpreter(object):
 
     def factor(self):
         token = self.current_token
-        self.eat(INTEGER)
-        return token.value
+        if token.type == INTEGER:
+            self.eat(INTEGER)
+            return token.value
+        elif token.type == LPAREN:
+            self.eat(LPAREN)
+            result = self.expr()
+            self.eat(RPAREN)
+            return result
 
     def term(self):
         result = self.factor()
