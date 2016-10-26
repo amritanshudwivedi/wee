57c6a144dddc5ca769667ad65c305acefb69e0a3
Amritanshu <amritanshu@grappus.com>
2016-10-26 09:04:02 +0530

    expressions with operator presedence

--- interpreter.py
+++ interpreter.py
@@ -104,25 +104,34 @@ class Interpreter(object):
         self.eat(INTEGER)
         return token.value
 
-    def expr(self):
-
-        # we expect the current token to be a single-digit integer
+    def term(self):
         result = self.factor()
-
-        while self.current_token.type in (PLUS, MINUS, MULTIPLY,DIVIDE):
+        while self.current_token.type in (MULTIPLY, DIVIDE):
             token = self.current_token
-            if token.type == PLUS:
-                self.eat(PLUS)
-                result += self.factor()
-            elif token.type == MINUS:
-                self.eat(MINUS)
-                result -= self.factor()
-            elif token.type == MULTIPLY:
+            if token.type == MULTIPLY:
                 self.eat(MULTIPLY)
                 result *= self.factor()
             elif token.type == DIVIDE:
                 self.eat(DIVIDE)
                 result /= self.factor()
+        return result
+
+    def expr(self):
+        result = self.term()
+        while self.current_token.type in (PLUS, MINUS):
+            token = self.current_token
+            if token.type == PLUS:
+                self.eat(PLUS)
+                result += self.term()
+            elif token.type == MINUS:
+                self.eat(MINUS)
+                result -= self.term()
+            elif token.type == MULTIPLY:
+                self.eat(MULTIPLY)
+                result *= self.term()
+            elif token.type == DIVIDE:
+                self.eat(DIVIDE)
+                result /= self.term()
         return result
 
 
